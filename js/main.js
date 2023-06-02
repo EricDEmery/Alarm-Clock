eec89742b2071feae65b24dbb38bef586d1e7c55
EricDEmery <ericemery099@gmail.com>
2023-06-02 09:35:14 -0400

    final commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
+function formatTime(time) {
+    if ( time < 10 ) {
+       /*console.log(`0${time}`)*/
+        return `0${time}`
+    }
+    return time;
+}
+
 function updateTime() {
     const display = document.getElementById('clock');
     const date = new Date();
         /* For Test console.log(display)*/
-    const hour = date.getHours();
+    let hour = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
         /* For Test console.log(hour)
         For Test console.log(minutes)
         For Test console.log(seconds)*/
-    display.innerText=`${hour} : ${minutes} : ${seconds}`
+    /*formatTime(hour)*/
+   /*console.log(hour)*/
+         hour = formatTime(hour)
+        let displayTime=`${hour} : ${minutes} : ${seconds}` 
+        display.innerText= displayTime
 }
 
-function formatTime(time) {
-    if ( time < 10 ) {
-        return `0${time}`
-    }
-    return time;
-}
 
 setInterval(updateTime, 1000);
