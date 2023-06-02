b8130522f191b11b4ae5fd408b294e23007c9557
EricDEmery <ericemery099@gmail.com>
2023-06-01 22:56:49 -0400

    fixed JS issues

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-const display = document.getElementById('clock');
-console.log(display)
 function updateTime() {
+    const display = document.getElementById('clock');
     const date = new Date();
-
+        console.log(display)
     const hour = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-console.log(hour)
-console.log(minutes)
-console.log(seconds)
+        console.log(hour)
+        console.log(minutes)
+        console.log(seconds)
     display.innerText=`${hour} : ${minutes} : ${seconds}`
 }
 
 function formatTime(time) {
     if ( time < 10 ) {
-        return '0' = time
+        return `0${time}`
     }
     return time;
 }
