186c40b15b69023753b99752d9cbf00701e30ebc
EricDEmery <ericemery099@gmail.com>
2023-06-01 22:47:39 -0400

    updated index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const display = document.getElementById('clock');
-
+console.log(display)
 function updateTime() {
     const date = new Date();
 
     const hour = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-
+console.log(hour)
+console.log(minutes)
+console.log(seconds)
     display.innerText=`${hour} : ${minutes} : ${seconds}`
 }
 
